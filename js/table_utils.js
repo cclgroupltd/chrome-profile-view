e2e2dcb8ddcc43665f363476e0f6c13a66f7b09d
Alex Caithness <Alex.Caithness@cclsolutionsgroup.com>
2024-06-25 19:43:15 +0100

    Fix class not being set in table_utils.js

--- js/table_utils.js
+++ js/table_utils.js
@@ -1,7 +1,7 @@
 function object2tr(obj, addClass=false, ...properties) {
     let cells;
     if(addClass){
-        cells = properties.map(x => $("<td></td>").attr("class", "x").text(obj[x]));
+        cells = properties.map(x => $("<td></td>").attr("class", x).text(obj[x]));
     }else{
         cells = properties.map(x => $("<td></td>").text(obj[x]));
     }
